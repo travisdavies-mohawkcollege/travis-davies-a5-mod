648d721df5e137bee50259520a11bf1fe2888c45
travisdavies-mohawkcollege <travis.davies@mohawkcollege.ca>
2025-04-16 15:48:20 -0400

    Setup multiple arenas and layer based hitboxes

---
+++ AsteroidsMod/Assets/Scripts/HyperspaceManager.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class HyperspaceManager : MonoBehaviour
+{
+    private int currentArena = 1;
+    public GameObject player1;
+    public GameObject player2;
+    public GameObject player3;
+    public GameObject player4;
+    public GameObject camera1;
+    public GameObject camera2;
+    public GameObject camera3;
+    public GameObject camera4;
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            currentArena++;
+            if (currentArena > 4)
+            {
+                currentArena = 1;
+            }
+        }
+        ArenaSwitcher();
+    }
+
+    private void ArenaSwitcher()
+    {
+        if (currentArena == 1)
+        {
+            player1.layer = 6;
+            player2.layer = 7;
+            player3.layer = 7;
+            player4.layer = 7;
+            camera1.SetActive(true);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+            camera4.SetActive(false);
+        }
+        else if (currentArena == 2)
+        {
+            player1.layer = 7;
+            player2.layer = 6;
+            player3.layer = 7;
+            player4.layer = 7;
+            camera1.SetActive(false);
+            camera2.SetActive(true);
+            camera3.SetActive(false);
+            camera4.SetActive(false);
+        }
+        else if(currentArena == 3)
+        {
+            player1.layer = 7;
+            player2.layer = 7;
+            player3.layer = 6;
+            player4.layer = 7;
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(true);
+            camera4.SetActive(false);
+        }
+        else
+        {
+            player1.layer = 7;
+            player2.layer = 7;
+            player3.layer = 7;
+            player4.layer = 6;
+            camera1.SetActive(false);
+            camera2.SetActive(false);
+            camera3.SetActive(false);
+            camera4.SetActive(true);
+        }
+    }
+
+
+    
+
+}
